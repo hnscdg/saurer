c05e9b63c9014c534e73378963b3daf0d24c92c2
Zhu <scn40006@sts.saurer.vpn>
2020-03-08 17:45:55 +0800

    change scss file to less

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,27 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { LocalStorageService } from '../services/local-storage/local-storage.service';
-import { INIT_FLAG, START_USING_DATE, USERNAME } from '../services/local-storage/local-storage.namespace';
-import { getTodayTime } from '../utlis/time';
+
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
-  styleUrls: ['./account.component.less']
+  styleUrls: ['./account.component.scss']
 })
 export class AccountComponent implements OnInit {
 
-  username: string;
-
-  constructor(private store: LocalStorageService) {
-   }
+  constructor() { }
 
   ngOnInit(): void {
   }
-
-  // completeSetup(): void {
-  //   console.log(this.username)
-  //   this.store.set(INIT_FLAG, true)
-  //   this.store.set(START_USING_DATE, getTodayTime());
-  //   this.store.set(USERNAME, this.username);
-  // }
 
 }
--- src/app/account/account.module.ts
+++ src/app/account/account.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AccountComponent } from './account.component';
-import { AccountRoutingModule } from './account-routing.module';
+
+
 
 @NgModule({
   declarations: [AccountComponent],
   imports: [
-    CommonModule,
-    AccountRoutingModule
-  ],
-  exports: [AccountModule]
+    CommonModule
+  ]
 })
 export class AccountModule { }
